164b8170dfdbc5b61603ef54a8bc63a30d6505ad
MRSherlock1308 <shubhamrai_se21a15_33@dtu.ac.in>
2023-05-20 00:22:20 +0530

    change in the package json and in the api folder

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "50044fa9de1e35cda87fff35d75fceac",
   base: "https://api.openweathermap.org/data/2.5/",
 };
